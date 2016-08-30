3e083c64059bced4ae2429ccd50d2e68d869c01e
Orlando Sergio Dominguez <serggio@msn.com>
2016-08-30 17:22:36 -0300

    Versión 0.0.14: Integracion a servicio proxy y flickr.

--- public/application/application.js
+++ public/application/application.js
@@ -1,7 +1,6 @@
 angular
 	.module('diputado',['ui.materialize','ngRoute','ngSession','ngLocal'])
-	.config(function($routeProvider,$httpProvider){
-		$httpProvider.defaults.useXDomain = true;
+	.config(function($routeProvider){
 	    $routeProvider
 	    	.when('/',{
 	    		redirectTo:'/diputados'
@@ -27,4 +26,62 @@ angular
 		        controller:'diputado'
 		    })
 		    .otherwise({redirectTo:'/diputados'});
+	})
+	.service('$httpX',function(){
+		_this     = this;
+		_this.url = null;
+		_this.get = function(url){
+			_this.url = encodeURI(url);
+			return _this.r;
+		};
+		_this.r   = {
+			success:function(callback){
+				string = 'Cadena de datos';
+				if('function' === typeof callback){
+					$.ajax({
+					  url:_this.url,
+					  type:'GET',
+					  dataType:'jsonp',
+					  //crossDomain:true,
+					  success:function(r,response,x) {
+					  	/*
+					  	response = HttpResponse(json.dumps('{"status" : "success"}'));
+						response.__setitem__("Content-type", "application/json");
+						response.__setitem__("Access-Control-Allow-Origin", "*");
+						*/
+						console.log(r,response,x);
+					  	callback(response);
+					  }
+					});
+					
+					/*
+					//iframe = document.createElement('<iframe id="iframeHttpX" src="'+_this.url+'" style="display:none;"></iframe>');
+					iframe = document.createElement('IFRAME');
+					iframe.id = 'iframeHttpX';
+					iframe.style = 'display:block;width:500px;height:500px;z-index:1000;';
+					iframe.src = _this.url;
+					//iframe.srcdoc = _this.url;
+					document.body.appendChild(iframe);
+					console.log(_this.url);
+					$('#iframeHttpX').on('load',function(){
+						console.log('holis');
+						htmlDoc = $('#iframeHttpX').html();
+						htmlDoc = window['iframeHttpX'].contentWindow;
+						
+					});
+					*/
+					/*
+					inlineElement.onload = function(){
+						console.log(inlineElement.contentWindow.document);
+					};
+					*/
+					
+				}
+				return _this.r;
+			},
+			error:function(callback){
+				if('function' === typeof callback) callback();
+				return _this.r;
+			}
+		};
 	});
--- public/controllers/fotografias.js
+++ public/controllers/fotografias.js
@@ -1,30 +1,19 @@
 angular
 	.module('diputado')
-	.controller('fotografias',function($scope,$http,$session,$routeParams){
+	.controller('fotografias',function($scope,$http,$httpX,$session,$routeParams){
 		
-		$scope.uriname = $routeParams.uriname;
+		$scope.uriname  = $routeParams.uriname;
 		$scope.diputado = JSON.parse($session.get('diputado'));
 
 		$scope.init = function(){
 
 			if($scope.diputado.flickr.match('www.flickr.com')!=null){
-
-				iframe = document.createElement('iframe');
-				iframe.src=$scope.diputado.flickr;
-				iframe.onload=function(){
-					console.log('Se a cargado un html');
-				};
-
-				console.log(iframe);
-				/*
-				$http.get($scope.diputado.flickr)
-					.success(function(htmldoc){
-						console.log(htmldoc);
-					})
-					.error(function(e){
-						console.log(e);
+				uri = $scope.diputado.flickr.replace('https://','').split('/').join('.-.');
+				$http
+					.get('https://legproxy.herokuapp.com/flickr/'+uri)
+					.success(function(json){
+						$scope.fotografias = json;
 					});
-					*/
 			}
 			
 		};
