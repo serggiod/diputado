6d1522a3f4b20d70650e51066f8095aaea7eb303
Orlando Sergio Dominguez <serggio@msn.com>
2017-10-04 10:17:22 -0300

    0.1.6: Listo para producción 2.

--- public/controllers/diputado.js
+++ public/controllers/diputado.js
@@ -2,20 +2,20 @@ angular
     .module('legislaturaweb')
     .controller('diputado', function($http, $scope, $location) {
         $scope.init = function(){
-            var loc = document.location.href || window.location.href;
-            var index  = loc.indexOf('/#/') +3;
+            var loc   = document.location.href || window.location.href;
+            var index = loc.indexOf('/#/') +3;
             $scope.uriname = loc.substr(index);
-            index=$scope.uriname.indexOf('/')
+            index=$scope.uriname.indexOf('/');
             if(index>=1) $scope.uriname = $scope.uriname.substr(0,index);
-            uri = '/rest/diputados.php/diputado/' + $scope.uriname;
+            uri = '/rest/diputado.php/' + $scope.uriname + '/perfil';
             $http
                 .get(uri)
                 .success(function(json) {
                     if (json.result === true) {
-                        json.rows.bloque = json.rows.bloque.replace('-', ' ').toUpperCase();
+                        json.rows.bloque = json.rows.bloque.replace(/-/g, ' ').toUpperCase();
                         for (i = 0; i < json.rows.comisiones.length; i++) {
                             comision = json.rows.comisiones.shift();
-                            comision.comision = comision.comision.replace('-', ' ').toUpperCase();
+                            comision.comision = comision.comision.replace(/-/g, ' ').toUpperCase();
                             json.rows.comisiones.push(comision);
                         }
                         $scope.model = json.rows;
